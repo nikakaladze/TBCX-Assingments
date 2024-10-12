d220d2a5cab627e55e66b9b6b5316f9c1cc80337
nikakaladze <nikakaladze89@gmail.com>
2024-10-12 16:59:35 +0400

    I moved the blogs page to the server component

--- src/app/blog/[slug]/page.js
+++ src/app/blog/[slug]/page.js
@@ -1,16 +1,18 @@
+// Generate static params for static generation of each post
 export async function generateStaticParams() {
   const response = await fetch('https://dummyjson.com/posts');
   const data = await response.json();
 
   return data.posts.map((post) => ({
-    slug: post.id.toString(),
+    slug: post.id.toString(),  // Generate slug for each post
   }));
 }
 
+// Server-side component for rendering post details
 export default async function PostDetail({ params }) {
   const { slug } = params;
 
-
+  // Fetch the post details from the API
   const response = await fetch(`https://dummyjson.com/posts/${slug}`);
   const post = await response.json();
 
@@ -18,13 +20,14 @@ export default async function PostDetail({ params }) {
     <div className="container">
       <h1 className="title">{post.title}</h1>
       <p className="body">{post.body}</p>
-      
 
+      {/* Display reactions */}
       <div className="reactions">
         <span>Likes: {post.reactions.likes}</span>
         <span>Dislikes: {post.reactions.dislikes}</span>
       </div>
-      
+
+      {/* Display views and tags */}
       <p className="views">Views: {post.views}</p>
       <ul className="tags">
         {post.tags.map((tag, index) => (
@@ -34,5 +37,4 @@ export default async function PostDetail({ params }) {
     </div>
   );
 }
-
 
